817dc4e6979cdf1480c9896564f14d7dee88ef71
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-03-30 22:51:46 +0900

    add a test

---
+++ _testdata/test1.go
@@ -0,0 +1,12 @@
+package testdata
+
+func Func1(arg1 string) error { return nil }
+
+// Func0 has no parameters.
+func Func0() {}
+
+var (
+	VarFoo  int
+	VarBar  string
+	varPriv float64
+)
--- read_test.go
+++ read_test.go
@@ -6,6 +6,8 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/koron-go/srcdom"
 )
 
@@ -49,4 +51,107 @@ func TestReadGOROOT(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestReadFile(t *testing.T) {
+	got, err := srcdom.Read(filepath.Join("_testdata", "test1.go"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := srcdom.Package{
+		Name: "testdata",
+		Funcs: []*srcdom.Func{
+			{
+				Name:    "Func1",
+				Params:  []*srcdom.Var{{Name: "arg1", Type: "string"}},
+				Results: []*srcdom.Var{{Type: "error"}},
+			},
+			{Name: "Func0"},
+		},
+		Values: []*srcdom.Value{
+			{Name: "VarFoo", Type: "int"},
+			{Name: "VarBar", Type: "string"},
+			{Name: "varPriv", Type: "float64"},
+		},
+	}
+	if d := cmp.Diff(&want, got, cmpopts.IgnoreUnexported(srcdom.Package{})); d != "" {
+		t.Errorf("unmatch srcdom.Package: -want +got\n%s", d)
+	}
+	pkg := got
+
+	t.Run("Value", func(t *testing.T) {
+		for _, c := range []struct {
+			name  string
+			ispub bool
+			want  srcdom.Value
+		}{
+			{"VarFoo", true, srcdom.Value{Name: "VarFoo", Type: "int"}},
+			{"VarBar", true, srcdom.Value{Name: "VarBar", Type: "string"}},
+			{"varPriv", false, srcdom.Value{Name: "varPriv", Type: "float64"}},
+		} {
+			got, ok := pkg.Value(c.name)
+			if !ok {
+				t.Errorf("value:%s not found", c.name)
+				continue
+			}
+			if d := cmp.Diff(&c.want, got); d != "" {
+				t.Errorf("value:%s unmatch: -want +got\n%s", c.name, d)
+			}
+			gotPub := got.IsPublic()
+			if gotPub != c.ispub {
+				t.Errorf("value:%s IsPublic unmatch: want=%t got=%t", c.name, c.ispub, gotPub)
+			}
+		}
+	})
+
+	t.Run("ValueNames", func(t *testing.T) {
+		got := pkg.ValueNames()
+		want := []string{
+			"VarBar",
+			"VarFoo",
+			"varPriv",
+		}
+		if d := cmp.Diff(want, got); d != "" {
+			t.Errorf("unmatch ValueNames() result: -want +got\n%s", d)
+		}
+	})
+
+	t.Run("Func", func(t *testing.T) {
+		for _, c := range []struct {
+			name  string
+			ispub bool
+			want  srcdom.Func
+		}{
+			{"Func1", true, srcdom.Func{
+				Name:    "Func1",
+				Params:  []*srcdom.Var{{Name: "arg1", Type: "string"}},
+				Results: []*srcdom.Var{{Type: "error"}},
+			}},
+			{"Func0", true, srcdom.Func{Name: "Func0"}},
+		} {
+			got, ok := pkg.Func(c.name)
+			if !ok {
+				t.Errorf("func:%s not found", c.name)
+				continue
+			}
+			if d := cmp.Diff(&c.want, got); d != "" {
+				t.Errorf("func:%s unmatch: -want +got\n%s", c.name, d)
+			}
+			gotPub := got.IsPublic()
+			if gotPub != c.ispub {
+				t.Errorf("func:%s IsPublic unmatch: want=%t got=%t", c.name, c.ispub, gotPub)
+			}
+		}
+	})
+
+	t.Run("FuncNames", func(t *testing.T) {
+		got := pkg.FuncNames()
+		want := []string{
+			"Func0",
+			"Func1",
+		}
+		if d := cmp.Diff(want, got); d != "" {
+			t.Errorf("unmatch FuncNames() result: -want +got\n%s", d)
+		}
+	})
 }
